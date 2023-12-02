b2656e6190fb48c8623a36f87ac6f3239991e55d
Ethan Jurman <ethanjurman@gmail.com>
2023-12-02 11:09:45 -0500

    scheduler to not go under 50 or over 1000

--- server/scheduler.js
+++ server/scheduler.js
@@ -6,7 +6,15 @@ async function updatePrices() {
   const updatedCookieData = Object.keys(cookieData).reduce((updated, key) => {
     const trendUp = cookieData[key].price > cookieData[key].previousPrice;
     const oldPrice = cookieData[key].price;
-    const newPrice = oldPrice + (Math.random() * 10) + (trendUp ? - 6 : - 4); // if it's trending up, have a higher chance to decrease
+    let newPrice = oldPrice + (Math.random() * 10) + (trendUp ? - 6 : - 4); // if it's trending up, have a higher chance to decrease
+    // the price should never be less than 50;
+    if (newPrice < 50) {
+      newPrice = 49.99;
+    }
+    // the price should never be more than 1000
+    if (newPrice > 1000) {
+      newPrice = 999.99;
+    }
     updated[key].price = Math.round(newPrice * 100) / 100;
     updated[key].previousPrice = oldPrice;
     return updated;
